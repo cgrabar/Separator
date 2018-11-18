fb9797a9370fb40f80e171871aa874e81a5f3210
Catherine Grabar <cgrabar@WASEA-MLDESCG.domain>
2018-11-18 11:21:44 -0800

    fixed coordinate bug

--- my-plugin/src/my-command.js
+++ my-plugin/src/my-command.js
@@ -1,5 +1,6 @@
 var sketch = require('sketch')
 // documentation: https://developer.sketchapp.com/reference/api/
+log("debug info")
 log(sketch.version.api)
 log(sketch.version.sketch)
 
@@ -14,7 +15,7 @@ export default function() {
 
   var selection = context.selection;
   var selectedLayers = context.selection;
-  //var selectedLayers = doc.selectedLayers();
+
   var bounds= MSLayerGroup.groupBoundsForContainer(MSLayerArray.arrayWithLayers(selection));
 
 
@@ -27,48 +28,48 @@ export default function() {
     var width = 1;
     var height = bounds.size.height;
   }
-  //var width = bounds.size.width;
-  //var height = 1;
+
   log("x: "+bounds.origin.x);
   log("y: "+bounds.origin.y);
   log("width: "+bounds.size.width);
   log("height: "+bounds.size.height);
 
+  var firstLayer = selection.firstObject();
+  var container  = firstLayer;
 
-selectedLayers.forEach(function (layer) {
-  log("debug info")
+  while (container.class() != 'MSPage' && container.class() != 'MSArtboardGroup' && container.class() != 'MSLayerGroup') {
+    container = container.parentGroup();
+  }
+
+
+  selectedLayers.forEach(function (layer) {
   log(layer.frame.width)
   layer.removeFromParent();
-})
-
+  })
 
   var myStyle = new sketch.Style()
-  myStyle.fills = ['#767676'];
+  myStyle.fills = ['#979797'];
   myStyle.borders = [{
     enabled: false
-    //thickness: 0
   }];
 
   var rect = new Shape({
-    parent: page,
+    parent: container,
     frame: new Rectangle(x, y, width, height),
     style: myStyle
   })
 
 
+  log("rect x:"+x)
+  log("rect y:"+y)
 
   var selection = document.selectedLayers
-
-  log(selection.isEmpty)
   selection.forEach(function(item) {
-    log(item.name)
   })
 
   selection.clear()
-  log(selection.isEmpty)
-
   rect.selected = true
-  log(selection.isEmpty)
+
 
 
 
