5d37c50f20520bfbfe56473fcba128d1bc86ee46
Catherine Grabar <cgrabar@wasea-mldescg.tasernet.com>
2018-11-20 14:57:52 -0800

    adding color transfer from line to rect

--- my-plugin/src/my-command.js
+++ my-plugin/src/my-command.js
@@ -9,16 +9,28 @@ export default function() {
   var page = document.selectedPage
   var doc = context.document;
 
-  var Group = sketch.Group
-  var Shape = sketch.Shape
-  var Rectangle = sketch.Rectangle
+  var Group = sketch.Group;
+  var Shape = sketch.Shape;
+  var Rectangle = sketch.Rectangle;
 
   var selection = context.selection;
   var selectedLayers = context.selection;
-
   var bounds= MSLayerGroup.groupBoundsForContainer(MSLayerArray.arrayWithLayers(selection));
 
+if (selection.count() == 0) {
+	doc.showMessage("Please select something.");
+} else {
+	for (var i = 0; i < selection.count(); i++) {
+		var layer = context.selection[i];
+		var borderColor = layer.style().firstEnabledBorder().color().immutableModelObject().hexValue();
+	}
+}
 
+log("border color: "+borderColor);
+var hexbordercolor = ("#" + borderColor);
+log ("border color2: "+hexbordercolor);
+
+//checking orientation of the line
   var x = bounds.origin.x;
   var y = bounds.origin.y;
   if (bounds.size.width > bounds.size.height) {
@@ -37,22 +49,30 @@ export default function() {
   var firstLayer = selection.firstObject();
   var container  = firstLayer;
 
+
+//selecting correct parent group
   while (container.class() != 'MSPage' && container.class() != 'MSArtboardGroup' && container.class() != 'MSLayerGroup') {
     container = container.parentGroup();
   }
 
 
+
   selectedLayers.forEach(function (layer) {
-  log(layer.frame.width)
-  layer.removeFromParent();
+  //  log(layer.frame.width)
+    layer.removeFromParent();
   })
 
+//setting style of rectangle fill to match line color and disabling rectangle border
   var myStyle = new sketch.Style()
-  myStyle.fills = ['#979797'];
+  myStyle.fills = hexbordercolor ? [hexbordercolor] : ['#979797'];
+  //myStyle.fills =  [hexbordercolor];
   myStyle.borders = [{
     enabled: false
   }];
 
+  log(myStyle);
+
+//draw rectangle
   var rect = new Shape({
     parent: container,
     frame: new Rectangle(x, y, width, height),
@@ -63,15 +83,13 @@ export default function() {
   log("rect x:"+x)
   log("rect y:"+y)
 
+
   var selection = document.selectedLayers
   selection.forEach(function(item) {
   })
 
   selection.clear()
   rect.selected = true
-
-
-
 
 
 }
